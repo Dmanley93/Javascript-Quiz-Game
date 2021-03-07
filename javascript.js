bd60bbd737a314fee835cf35bc0522430a394f24
Dmanley93 <77508379+Dmanley93@users.noreply.github.com>
2021-03-06 18:16:31 -0600

    Add files via upload
    updating javascript again

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 //Declaring my variables
 var startEl = document.querySelector('#start')
 var smallTextEl = document.querySelector('#smallText')
@@ -14,6 +15,7 @@ let currentQuestion = {};
 let availableQuestions = [];
 let questionCount = 0;
 let score = 0;
+var questionNumber;
 
 var secondsLeft = 75;
 var questions = [
@@ -67,7 +69,11 @@ function setTime() {
             clearInterval(timerInterval);
         }
     }, 1000)
+    if (questionNumber == questions.length) {
+    gameOver();
+  }
 }
+
 // function to hide the paragraph text below the h1
 function hideSmall() {
     smallTextEl.textContent = ' ';
@@ -125,6 +131,7 @@ function startGame(params) {
    console.log('you clicked the button!')
     questionCount = 0;
     score = 0;
+    questionNumber = 0;
     setTime();
     hideSmall();
     questionAsk();
@@ -143,179 +150,26 @@ function newQuestion() {
   question.innerText = currentQuestion.question;
 }
 
-function selectAnswer(params) {
-  
-}
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  if (!selectedButton.dataset.correct) {
+        secondsLeft = secondsLeft - 10;
+        console.log(score)
+      }
+    }
 
+function gameOver () {
+  clearInterval(secondsLeft);
+  secondsLeft = 75;
+  score = 0;
+}
 
 startEl.addEventListener("click", startGame)
 optionEl1.addEventListener('click', newQuestion)
+optionEl1.addEventListener('click', selectAnswer)
 optionEl2.addEventListener('click', newQuestion)
+optionEl2.addEventListener('click', selectAnswer)
 optionEl3.addEventListener('click', newQuestion)
+optionEl3.addEventListener('click', selectAnswer)
 optionEl4.addEventListener('click', newQuestion)
-
-
-// var startButton = document.getElementById('start-btn')
-// var nextButton = document.getElementById('next-btn')
-// var questionContainerElement = document.getElementById('question-container')
-// var questionElement = document.getElementById('question')
-// var answerButtonsElement = document.getElementById('answer-buttons')
-// var timeEl = document.getElementById('#setTime')
-// var highScore = document.getElementById('#highScore')
-// let shuffledQuestions, currentQuestionIndex
-// let timer = 75;
-// let runningTimer;
-// let score = 0;
-// let finalScore
-// secondsLeft = 5;
-// let Element1
-
-// //function to start the game clock
-// function startClock() {
-//   setTime.innerHTML = "Time Left = " + timer;
-//   if (timer <= 0) {
-//     gameOver();
-//   } else {
-//     timer--;
-//     runningTimer = setTimeout(startClock, 1000);
-//   }
-// }
-
-// //Event listeners to tell the start and next button what to
-// startButton.addEventListener('click', startGame)
-// nextButton.addEventListener('click', () => {
-//   currentQuestionIndex++
-//   setNextQuestion()
-// })
-
-// // funtion to start the game, hide necessary buttons, start the clock, and set the questions and answers
-// function startGame() {
-//   startButton.classList.add('hide')
-//   shuffledQuestions = questions.sort(() => Math.random() - .5);
-//   currentQuestionIndex = 0;
-//   questionContainerElement.classList.remove('hide');
-//   setNextQuestion();
-//   startClock();
-// }
-
-// //Function to set next question and shuffle questions
-// function setNextQuestion() {
-//   resetState();
-//   showQuestion(shuffledQuestions[currentQuestionIndex]);
-// }
-
-// //Function to display question
-// function showQuestion(question) {
-//   questionElement.innerText = question.question
-//   question.answers.forEach(answer => {
-//     const button = document.createElement('button');
-//     button.innerText = answer.text
-//     button.classList.add('btn');
-//     if (answer.correct) {
-//       button.dataset.correct = answer.correct
-//     }
-//     button.addEventListener('click', selectAnswer);
-//     answerButtonsElement.appendChild(button);
-//   })
-// }
-
-// // function to reset everything 
-// function resetState() {
-//   clearStatusClass(document.body);
-//   nextButton.classList.add('hide');
-//   while (answerButtonsElement.firstChild) {
-//     answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-//   }
-// }
-
-// function addScore(params) {
-//   document.getElementById('highScore').innerText = score;
-// }
-
-// //function to select answers and subtract time if answer is wrong.
-// function selectAnswer(e) {
-//   const selectedButton = e.target
-//   const correct = selectedButton.dataset.correct
-//   setStatusClass(document.body, correct);
-//   if (!selectedButton.dataset.correct) {
-//     timer = timer - 10;
-//   }
-//   if (selectedButton.dataset.correct) {
-//     score += 100
-//     console.log(score)
-//   }
-//   Array.from(answerButtonsElement.children).forEach(button => {
-//     setStatusClass(button, button.dataset.correct);
-//   })
-//   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-//     nextButton.classList.remove('hide');
-//   } 
-//   else {
-//     startButton.innerText = 'Restart'
-//     startButton.classList.remove('hide');
-//   }
-// }
-
-// //function to 
-// function setStatusClass(element, correct) {
-//   clearStatusClass(element)
-//   if (correct) {
-//     element.classList.add('correct');
-//   } else {
-//     element.classList.add('wrong');
-//   }
-// }
-
-// function clearStatusClass(element) {
-//   element.classList.remove('correct');
-//   element.classList.remove('wrong');
-// }
-
-// //function to end the game
-// function gameOver() {
-//   clearInterval(runningTimer);
-//   setTime.innerHTML = "0";
-//   startButton.innerText = "Restart";
-//   startButton.classList.remove("hide");
-//   timer = 90;
-// }
-
-// //available questions
-// const questions = [
-//   {
-//     question: 'What is 2 + 2?',
-//     answers: [
-//       { text: '4', correct: true },
-//       { text: '22', correct: false },
-//       { text: '53', correct: false},
-//       { text: '16', correct:false}
-//     ]
-//   },
-//   {
-//     question: 'What is 4 x 4?',
-//     answers: [
-//       { text: '4', correct: false },
-//       { text: '65', correct: false },
-//       { text: '16', correct: true },
-//       { text: '32', correct: false }
-//     ]
-//   },
-//   {
-//     question: 'What is 5 + 3?',
-//     answers: [
-//       { text: '8', correct: true },
-//       { text: '23', correct: false },
-//       { text: '16', correct: false },
-//       { text: '3', correct: false }
-//     ]
-//   },
-//   {
-//     question: 'What is 4 * 2?',
-//     answers: [
-//       { text: '6', correct: false },
-//       { text: '8', correct: true },
-//       { text: '12', correct: false},
-//       { text: '18', correct: false}
-//     ]
-//   }
-// ]
+optionEl4.addEventListener('click', selectAnswer)
